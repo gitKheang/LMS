66ed7b736e220ba62eaf8d9ad319e2d3e303d036
khorn kimkhimmengkheang <kimkhimmengkheang.khorn22@kit.edu>
2025-12-01 21:32:45 +0700

    Format code

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -15,10 +15,13 @@ app.use((req, res, next) => {
   if (origin) {
     res.setHeader("Access-Control-Allow-Origin", origin);
   }
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.setHeader("Access-Control-Allow-Credentials", "true");
-  
+
   // Handle preflight
   if (req.method === "OPTIONS") {
     res.status(200).end();
